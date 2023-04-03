8212f8bd4870464481c92696837c538e67158da4
Damtab83 <damtab@hotmail.com>
2023-04-03 23:05:12 +0200

    create save & restore database, place contraint.

--- cinema_BDD.sql
+++ cinema_BDD.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS 'eval_BDD';
+CREATE DATABASE IF NOT EXISTS 'eval_BDD'CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
 
@@ -9,7 +9,7 @@ CREATE TABLE 'cinema' (
   nom VARCHAR(100) NOT NULL,
   nb_salles INT(11) NOT NULL,
   localisation VARCHAR(255),
-  IdAdmin INT(11),
+  IdAdmin INT(11) NOT NULL,
   FOREIGN KEY (IdAdmin) REFERENCES admin(id)
 ) ENGINE = InnoDB;
 
@@ -63,7 +63,7 @@ CREATE TABLE salle (
   id INT(11) NOT NULL AUTO_INCREMENT,
   numero_salle INT(11) NOT NULL,
   nb_places INT(100),
-  IdCinema INT(11),
+  IdCinema INT(11) NOT NULL,
   FOREIGN KEY (IdCinema) REFERENCES cinema (id) ON DELETE CASCADE
 )  ENGINE = InnoDB;
 
@@ -76,20 +76,28 @@ VALUES
   (4, 450),
 
 --   ADMIN
--- Création de la table admin
-CREATE TABLE admin (
+-- Création de la table admincine
+CREATE TABLE admincine (
   id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL,
+  prenom VARCHAR(255) NOT NULL,
   role_type VARCHAR(255) NOT NULL;
 ) ENGINE = InnoDB;
 
---Création des données admin
-INSERT INTO admin (nom, role)
+ALTER TABLE admincine
+  ADD nom VARCHAR(255) NOT NULL
+  AFTER id;
+  ADD password VARCHAR(50)
+  AFTER role_type;
+
+--Création des données admincine
+INSERT INTO admincine (prenom, role)
 VALUES
-  ('Clark', 'vendeur'),
-  ('Amélie', 'vendeuse'),
-  ('Franck', 'assitant direction'),
-  ('Fabienne', 'directrice'),
+  ('Ken', 'Clark', 'vendeur',),
+  ('Poule', 'Amélie', 'vendeuse'),
+  ('Sinistre', 'Franck', 'assitant direction'),
+  ('Legal', 'Fabienne', 'directrice'),
+
+
 
 --   SEANCE
 -- Création de la table séance
@@ -99,7 +107,7 @@ CREATE TABLE seance (
   nb_places_dispo INT(100) NOT NULL,
   IdSalle INT(11),
   FOREIGN KEY (IdSalle) REFERENCES salle (id),
-  IdFilm INT(11),
+  IdFilm INT(11) NOT NULL,
   FOREIGN KEY (IdFilm) REFERENCES film (id)
 ) ENGINE = InnoDB;
 
@@ -109,8 +117,8 @@ INSERT INTO seance (horaire, nb_places_dispo)
 -- Création de la table billet
 CREATE TABLE billet (
   id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nb_billet INT(100)
-  IdTarifs INT(11),
+  nb_billet INT(100) NOT NULL,
+  IdTarifs DECIMAL(10,2) NOT NULL,
   FOREIGN KEY (IdTarifs) REFERENCES tarifs(id),
   IdSeance INT(11),
   FOREIGN KEY (IdSeance) REFERENCES seance (id)
@@ -124,3 +132,25 @@ IF (SELECT nb_places_dispo FROM seance WHERE id=billet.IdSeance) >= billet.nb_bi
 
 -- Pour vider la table de ses données executuer:
 --     TRUNCATE TABLE MaTable
+
+
+/*//////////// LIGNES DE COMMANDES SUPPLEMENTAIRES ////////////*/
+/* commande sauvegarde bdd--------------------------------------------------------------------------------
+mysqldump -u root -p --databases eval_BDD > dump_eval_BDD.sql
+ */
+
+ /* commande restauration bdd--------------------------------------------------------------------------------
+mysql -u root -p eval_BDD < dump_eval_BDD.sql
+ */
+
+/* commande creation utilisateur--------------------------------------------------------------------------
+   CREATE USER 'admin'@'localhost' IDENTIFIED BY '$2y$10$o8za0K8NDjBKP9ZJ/LHpBetB5JagFpQ3EUYyBAwFh8y03O6gH/UyW';
+   
+   CREATE USER 'vendeurs'@'localhost' IDENTIFIED BY '$2y$10$E3HgXokKT6xrhfY1t.7Xf.zeJm.usQre1mw01cJBo5cTUs87Dc7ze';
+ */
+
+/* parametrer les privilieges utilisateurs------------------------------------------------------------
+   -----------------tous les droits (lecture/ecriture) pour l'admin------------------------------------
+   GRANT ALL PRIVILEGES ON eval_BDD.* TO 'admin'@'localhost';
+   FLUSH PRIVILEGES
+ */
